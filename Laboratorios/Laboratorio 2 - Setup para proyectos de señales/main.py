77a5ed36c3d8d63e6f0e92a7991a07c4d60b05d7
alvaro <alvarodevcode@outlook.com>
2025-04-06 12:02:45 -0500

    feat: added both signals

--- Laboratorios/Laboratorio 2 - Setup para proyectos de señales/main.py
+++ Laboratorios/Laboratorio 2 - Setup para proyectos de señales/main.py
@@ -1,11 +1,16 @@
 import neurokit2 as nk
 import pandas as pd
 
-# Simulate
-emg2 = nk.emg_simulate(duration=10, burst_number=2, burst_duration=1.0)
-emg2_long = nk.emg_simulate(duration=10, burst_number=2, burst_duration=1.5)
-emg5 = nk.emg_simulate(duration=10, burst_number=5, burst_duration=1.0)
+# Simulated signals for EMG
+emgA_long = nk.emg_simulate(duration=15, burst_number=5, burst_duration=2)
+emgB_long = nk.emg_simulate(duration=15, burst_number=10, burst_duration=0.8)
 
-# Visualize
-emg_df = pd.DataFrame({"EMG2": emg2, "EMG2_Longer": emg2_long, "EMG5": emg5})
+# Simulated signals for ECG
+ecgA_long = nk.ecg_simulate(duration=15, heart_rate=70, noise=0.1)
+ecgB_long = nk.ecg_simulate(duration=15, heart_rate=55, noise=0.2)
+
+emg_df = pd.DataFrame({"EMG2_Long": emgA_long, "EMG5": emgB_long})
+ecg_df = pd.DataFrame({"ECG2_Long": ecgA_long, "ECG5": ecgB_long})
+
 nk.signal_plot(emg_df,subplots=True)
+nk.signal_plot(ecg_df,subplots=True)
